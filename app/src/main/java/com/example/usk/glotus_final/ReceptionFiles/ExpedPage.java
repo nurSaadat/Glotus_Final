1a248f5adc720391fa1d73be001b0a324c0098fa
wasdx19 <vanrwei@gmail.com>
2018-10-03 21:11:38 +0600

    added printer

--- app/src/main/java/com/example/usk/glotus_final/ReceptionFiles/ExpedPage.java
+++ app/src/main/java/com/example/usk/glotus_final/ReceptionFiles/ExpedPage.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.usk.glotus_final.R;
+import com.example.usk.glotus_final.SuperviserListFiles.SuperviserListActivity;
 import com.example.usk.glotus_final.SuperviserListFiles.SuperviserListItemActivity;
 
 import java.io.File;
@@ -29,6 +30,8 @@ public class ExpedPage extends AppCompatActivity {
     private RelativeLayout pdf_cont;
     private ScrollView scrollView2;
     private MenuItem btn_generate;
+    private MenuItem btn_ok;
+    private MenuItem btn_print;
     private PdfData item=Reception.pd;
     private String upak=Reception.upakovka.getSelectedItem().toString();
 
@@ -85,7 +88,13 @@ public class ExpedPage extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.pdf_menu, menu);
         btn_generate = menu.findItem(R.id.pdf_create);
-        btn_generate.setVisible(true);
+        btn_generate.setVisible(false);
+
+        btn_ok=menu.findItem(R.id.btn_ok);
+        btn_ok.setVisible(true);
+
+        btn_print=menu.findItem(R.id.btn_print);
+        btn_print.setVisible(true);
         return true;
     }
 
@@ -93,9 +102,17 @@ public class ExpedPage extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.pdf_create) {
-            save(pdf_cont);
+        if(id==R.id.btn_ok){
+
         }
+        if(id==R.id.btn_print){
+            Intent myintent = new Intent(ExpedPage.this, SuperviserListActivity.class);
+            startActivity(myintent);
+        }
+
+        /*if (id == R.id.pdf_create) {
+            save(pdf_cont);
+        }*/
         return super.onOptionsItemSelected(item);
     }
 

d0a28a2996394cd8861a4144da5cd8650afbf726
Ulukbek Samakov <ulukbek.samakov@gmail.com>
2019-05-20 10:41:33 +0300

    last

--- app/src/main/java/com/example/usk/glotus_final/SuperviserApp/ReceptionFiles/Etiketka.java
+++ app/src/main/java/com/example/usk/glotus_final/SuperviserApp/ReceptionFiles/Etiketka.java
@@ -74,7 +74,7 @@ public class Etiketka extends AppCompatActivity{
         Date c = Calendar.getInstance().getTime();
         System.out.println("Current time => " + c);
 
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
         String formattedDate = df.format(c);
 
         TextView gorodOtkuda=findViewById(R.id.gorod_otkuda);
